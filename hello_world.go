caf42c0369e3e7960a79e96953a2896dd3ec0610
Damon Nyhan <damon.nyhan@gmail.com>
2018-12-03 22:15:07 -0800

    commit from VSC

--- hello_world.go
+++ hello_world.go
@@ -1,5 +1,10 @@
 package main
 
+/**
+* main - prints hello world
+* return nothing
+ */
+
 func main() {
 	print("Hello World!\n")
 }
---
+++ memset.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func go_memset(a []int, v int) {
+	for i := range a {
+		a[i] = v
+	}
+}
+func simple_print_buffer()
+fun main() {
+
+}
